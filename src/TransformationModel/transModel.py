c77d63ea01e0c1d5cf8284ecd50e2f3338bf07d4
janha <jan.m.hanes@gmail.com>
2024-05-24 10:11:22 +0200

    added second version of MLP, connected, where we use one MLP for both quaternion and position deformation

--- src/TransformationModel/transModel.py
+++ src/TransformationModel/transModel.py
@@ -3,17 +3,10 @@ from torch import nn
 import torch
 
 device = 'cuda:0'
-coordinate_L = 10
-quaternion_L = 15
+coordinate_L = 10   #as per original nerf paper
+quaternion_L = 15   #no paper exists for this value, trial and error
 pos_dim = 3
 quat_dim = 4
-
-
-def build_model():
-    model = nn.Sequential(
-
-    )
-    return 0
 
 
 class DeformationNetworkSeperate(torch.nn.Module):
@@ -53,7 +46,7 @@ class DeformationNetworkSeperate(torch.nn.Module):
 
     def forward(self, x, q, t):
         if t == 0:
-            return np.zeros(pos_dim + quat_dim)
+            return torch.zeros(pos_dim + quat_dim)
         higher_x = higher_dim_gamma(x, coordinate_L)
         higher_x = torch.tensor(higher_x).to(device)
         input_x = torch.cat((higher_x, t), 0)
@@ -76,7 +69,6 @@ class DeformationNetworkSeperate(torch.nn.Module):
         higher_q = higher_dim_gamma(q, quaternion_L)
         higher_q = torch.tensor(higher_q).to(device)
         input_q = torch.cat((higher_q, t), 0)
-        input_q = torch.cat((x, t), 0)
         input_q = self.linear_q_1(input_q)
         input_q = self.relu_q_1(input_q)
         input_q = self.linear_q_2(input_q)
@@ -94,18 +86,66 @@ class DeformationNetworkSeperate(torch.nn.Module):
         input_q = self.linear_q_8(input_q)
 
         #normalization of q
-        input_q = normalize_q(input_q)
+        input_q = normalize_q_torch(input_q)
 
         return input_x, input_q
 
 
-def higher_dim_gamma(p, length_ar):
+class DeformationNetworkConnected(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.linear_1 = torch.nn.Linear(quaternion_L * 2 * quat_dim + coordinate_L*2* pos_dim + 1, 256)
+        self.linear_2 = torch.nn.Linear(256, 256)
+        self.linear_3 = torch.nn.Linear(256, 256)
+        self.linear_4 = torch.nn.Linear(256, 256)
+        self.linear_5 = torch.nn.Linear(256, 256)
+        self.linear_6 = torch.nn.Linear(256, 256)
+        self.linear_7 = torch.nn.Linear(256, 256)
+        self.linear_8 = torch.nn.Linear(256, pos_dim + quat_dim)
+        self.relu_1 = torch.nn.ReLU()
+        self.relu_2 = torch.nn.ReLU()
+        self.relu_3 = torch.nn.ReLU()
+        self.relu_4 = torch.nn.ReLU()
+        self.relu_5 = torch.nn.ReLU()
+        self.relu_6 = torch.nn.ReLU()
+        self.relu_7 = torch.nn.ReLU()
+
+    def forward(self, x, q, t):
+        if t == 0:
+            return torch.zeros(pos_dim + quat_dim)
+        higher_x = higher_dim_gamma(x, coordinate_L)
+        higher_q = higher_dim_gamma(q, quaternion_L)
+        higher_x = torch.tensor(higher_x).to(device)
+        higher_q = torch.tensor(higher_q).to(device)
+        input_total = torch.cat((higher_x, higher_q, t), 0)
+        input_total = self.linear_1(input_total)
+        input_total = self.relu_1(input_total)
+        input_total = self.linear_2(input_total)
+        input_total = self.relu_2(input_total)
+        input_total = self.linear_3(input_total)
+        input_total = self.relu_3(input_total)
+        input_total = self.linear_4(input_total)
+        input_total = self.relu_4(input_total)
+        input_total = self.linear_5(input_total)
+        input_total = self.relu_5(input_total)
+        input_total = self.linear_6(input_total)
+        input_total = self.relu_6(input_total)
+        input_total = self.linear_7(input_total)
+        input_total = self.relu_7(input_total)
+        input_total = self.linear_8(input_total)
+        out_x, out_q = torch.split(input_total, pos_dim)
+        out_q = normalize_q_torch(out_q)
+        return out_x, out_q
+
+
+
+def higher_dim_gamma(p, length_ar):  #as per original nerf paper
     sins = np.sin(np.outer(2 ** np.array(list(range(length_ar))) * np.pi, p))
     coss = np.cos(np.outer(2 ** np.array(list(range(length_ar))) * np.pi, p))
     return np.column_stack((sins, coss))
 
 
-def normalize_q(q):
+def normalize_q_torch(q):            #normalization of quaternion. Only unit quaternions can be interpreted as rotations.
     norm_q = torch.sum(q ** 2)
     norm_q = torch.sqrt(norm_q)
     return q / norm_q
