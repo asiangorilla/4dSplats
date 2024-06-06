bc6fe3f5c3198952e4b091ed3c38a8915cf208e6
eVaultra <118146933+eVaultra@users.noreply.github.com>
2024-06-06 21:02:48 +0200

    Update input.py

--- src/data_input/input.py
+++ src/data_input/input.py
@@ -1,1 +1,40 @@
+from plyfile import PlyData
+import numpy as np
+import torch
+from transModel import DeformationNetworkSeparate, DeformationNetworkConnected
+
+# read .ply file
+ply_path = 'F:\\Files\\Project\\point_cloud.ply'
+plydata = PlyData.read(ply_path)
+
+# Extracting point cloud data
+vertex_data = plydata['vertex'].data
+points = np.vstack([vertex_data['x'], vertex_data['y'], vertex_data['z']]).T
+colors = np.vstack([vertex_data['f_dc_0'], vertex_data['f_dc_1'], vertex_data['f_dc_2']]).T
+features_rest = np.vstack([vertex_data['f_rest_{}'.format(i)] for i in range(45)]).T
+opacities = vertex_data['opacity']
+scales = np.vstack([vertex_data['scale_0'], vertex_data['scale_1'], vertex_data['scale_2']]).T
+rotations = np.vstack([vertex_data['rot_0'], vertex_data['rot_1'], vertex_data['rot_2'], vertex_data['rot_3']]).T
+
+# Converting Data to PyTorch Tensor
+points_tensor = torch.tensor(points, dtype=torch.float32).cuda()
+colors_tensor = torch.tensor(colors, dtype=torch.float32).cuda()
+features_rest_tensor = torch.tensor(features_rest, dtype=torch.float32).cuda()
+opacities_tensor = torch.tensor(opacities, dtype=torch.float32).cuda()
+scales_tensor = torch.tensor(scales, dtype=torch.float32).cuda()
+rotations_tensor = torch.tensor(rotations, dtype=torch.float32).cuda()
+
+# Model Instantiation
+model = DeformationNetworkSeparate()  # or DeformationNetworkConnected()
+model = model.cuda()
+
+# Setting of time t
+t = 0
+
+# Passing data to the transmodel
+output_x, output_q = model(points_tensor, rotations_tensor, t)
+
+# output
+print("Output positions:", output_x)
+print("Output quaternions:", output_q)
 
